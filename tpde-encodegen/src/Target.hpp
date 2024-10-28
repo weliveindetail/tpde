ed879bd78b4de669fa0f5f06a8ae7806243f7ab4
T0b1 <github@t0b1.dev>
2024-10-28 17:05:10 +1300

    [ENCODEGEN] clang-format Target.hpp

--- tpde-encodegen/src/Target.hpp
+++ tpde-encodegen/src/Target.hpp
@@ -18,26 +18,38 @@ struct MICandidate {
         //     NONE,
         //     IMM32S,
         //     IMM8S,
-        //     MEM_X86, // can replace register with x86 memory operand, if taken, operand is expanded to four values (base, scale, index, off)
-        //     ADDR_X86, // can replace register with x86 address (base+scaledidx+off)
-        //     ADDR_OFF12U0, // can replace register with addr+offset where offset is 12 bits unsigned, shifted left by 0 bits
-        //     ADDR_OFF12U1, // can replace register with addr+offset where offset is 12 bits unsigned, shifted left by 1 bits
-        //     ADDR_OFF12U2, // can replace register with addr+offset where offset is 12 bits unsigned, shifted left by 2 bits
-        //     ADDR_OFF12U3, // can replace register with addr+offset where offset is 12 bits unsigned, shifted left by 3 bits
+        //     MEM_X86, // can replace register with x86 memory operand, if
+        //     taken, operand is expanded to four values (base, scale, index,
+        //     off) ADDR_X86, // can replace register with x86 address
+        //     (base+scaledidx+off) ADDR_OFF12U0, // can replace register with
+        //     addr+offset where offset is 12 bits unsigned, shifted left by 0
+        //     bits ADDR_OFF12U1, // can replace register with addr+offset where
+        //     offset is 12 bits unsigned, shifted left by 1 bits ADDR_OFF12U2,
+        //     // can replace register with addr+offset where offset is 12 bits
+        //     unsigned, shifted left by 2 bits ADDR_OFF12U3, // can replace
+        //     register with addr+offset where offset is 12 bits unsigned,
+        //     shifted left by 3 bits
         // };
-        unsigned op_idx;
+        unsigned    op_idx;
         std::string cond_str;
 
         Cond() : op_idx(0) {}
-        Cond(unsigned op_idx, std::string cond_str) : op_idx(op_idx), cond_str(cond_str) {}
+
+        Cond(unsigned op_idx, std::string cond_str)
+            : op_idx(op_idx), cond_str(cond_str) {}
     };
 
-    using Generator = std::function<void(std::string &buf, const llvm::MachineInstr &mi, llvm::SmallVectorImpl<std::string> &ops)>;
-    Cond cond;
+    using Generator =
+        std::function<void(std::string                        &buf,
+                           const llvm::MachineInstr           &mi,
+                           llvm::SmallVectorImpl<std::string> &ops)>;
+    Cond      cond;
     Generator generator;
 
     MICandidate(Generator generator) : cond(), generator(generator) {}
-    MICandidate(unsigned op_idx, std::string cond_str, Generator generator) : cond(op_idx, cond_str), generator(generator) {}
+
+    MICandidate(unsigned op_idx, std::string cond_str, Generator generator)
+        : cond(op_idx, cond_str), generator(generator) {}
 };
 
 struct EncodingTarget {
@@ -48,8 +60,12 @@ struct EncodingTarget {
     virtual ~EncodingTarget() = default;
 
     virtual std::string_view get_invalid_reg() = 0;
-    // Get all encoding candidates, the candidate without conditions must be last
-    virtual void get_inst_candidates(llvm::MachineInstr &, llvm::SmallVectorImpl<MICandidate> &) {}
+
+    // Get all encoding candidates, the candidate without conditions must be
+    // last
+    virtual void get_inst_candidates(llvm::MachineInstr &,
+                                     llvm::SmallVectorImpl<MICandidate> &) {}
+
     virtual unsigned         reg_id_from_mc_reg(llvm::MCRegister)    = 0;
     virtual std::string_view reg_name_lower(unsigned id)             = 0;
     virtual unsigned         reg_bank(unsigned id)                   = 0;
@@ -64,4 +80,4 @@ struct EncodingTarget {
     virtual bool inst_should_be_skipped(llvm::MachineInstr &inst)    = 0;
 };
 
-} // end namespace tpde_encdgen
+} // namespace tpde_encgen
