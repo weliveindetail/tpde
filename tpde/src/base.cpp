0c4c83a12c783b0d0a8983307e5b9bfcce438123
Alexis Engelke <engelke@in.tum.de>
2025-01-20 18:34:19 +0100

    [TPDE] Fix compilation with logging disabled

--- tpde/src/base.cpp
+++ tpde/src/base.cpp
@@ -2,9 +2,11 @@
 
 #include "tpde/base.hpp"
 
+#include <cstdlib>
+
 namespace tpde {
 
-[[noreturn]] void fatal_error(const char *msg) noexcept {
+[[noreturn]] void fatal_error([[maybe_unused]] const char *msg) noexcept {
   TPDE_LOG_ERR("TPDE FATAL ERROR: {}", msg);
   abort();
 }
